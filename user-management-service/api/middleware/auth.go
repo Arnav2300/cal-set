5a4de533c683a012cccd636a0401b8873c2778f6
Arnav Saraswat <arnav.sar@gmail.com>
2024-11-03 08:39:26 +0000

    auth middleware completed

--- user-management-service/api/middleware/auth.go
+++ user-management-service/api/middleware/auth.go
@@ -1,18 +1,45 @@
 package middleware
 
 import (
+	"fmt"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/golang-jwt/jwt/v5"
 )
 
 func isAuthorized(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		if r.Header["Token"] == nil {
+		authHeader := r.Header.Get("Authorization")
+		if authHeader == "" {
 			http.Error(w, "No token found", http.StatusUnauthorized)
 			return
 		}
-
+		tokenString := strings.Split(authHeader, " ")[1]
+		signingKey := []byte(os.Getenv("SECRET_KEY"))
+		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				http.Error(w, "Error parsing token", http.StatusInternalServerError)
+				return nil, fmt.Errorf("Error parsing token")
+			}
+			return signingKey, nil
+		})
+		if err != nil {
+			http.Error(w, "Token expired", http.StatusUnauthorized)
+			return
+		}
+		if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+			role := claims["role"]
+			switch role {
+			case "admin":
+				r.Header.Set("role", "admin")
+				return
+			case "user":
+				r.Header.Set("role", "user")
+				return
+			}
+		}
+		next.ServeHTTP(w, r)
 	}
 }
